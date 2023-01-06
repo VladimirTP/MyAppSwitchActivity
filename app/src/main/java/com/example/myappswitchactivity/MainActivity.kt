11095545bf877e228357d0ac2e3d8011c3824e50
TamashovVladimir <v.tamashov@gmail.com>
2023-01-06 07:26:22 +0300

    homework_MyAppSwitchActivity

--- app/src/main/java/com/example/myappswitchactivity/MainActivity.kt
+++ app/src/main/java/com/example/myappswitchactivity/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.myappswitchactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myappswitchactivity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonCounter.setOnClickListener{
+            counter++
+            binding.tvCounter.text = counter.toString()
+        }
+
+        binding.buttonResult.setOnClickListener{
+            val numberFromFirstActivity = Intent(this, MainActivity2 ::class.java)
+            numberFromFirstActivity.putExtra("COUNTER", counter)
+            startActivity(numberFromFirstActivity)
+        }
     }
 }
--- app/src/main/java/com/example/myappswitchactivity/MainActivity2.kt
+++ app/src/main/java/com/example/myappswitchactivity/MainActivity2.kt
@@ -2,10 +2,18 @@ package com.example.myappswitchactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myappswitchactivity.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
+
+    lateinit var binding : ActivityMain2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val counterNumber = intent.getIntExtra("COUNTER", 0)
+        binding.tvStartCounter.text = counterNumber.toString()
     }
 }
